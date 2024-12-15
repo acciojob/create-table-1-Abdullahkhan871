78f1978b424532071731e84c2e95427d99378aa5
Abdullah khan <akhan283785@gmail.com>
2024-12-15 23:37:02 +0530

    try

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+  let table = document.querySelector("#sampleTable tbody");
+  //   console.log(table);
+
+  let tr = document.createElement("tr");
+  let tdOne = document.createElement("td");
+  let tdTwo = document.createElement("td");
+  tdOne.innerText = "New Cell1";
+  tdTwo.innerText = "New Cell2";
+  tr.append(tdOne, tdTwo);
+  table.prepend(tr);
 }
